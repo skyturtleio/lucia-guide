bb1c7b26a32ce709a0db0b8e7b62dfbf09a6a5db
Leo Mendoza Jr <dev@skyturtle.io>
2023-08-29 12:03:58 -0700

    update drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,7 +1,12 @@
 import type { Config } from 'drizzle-kit';
+import 'dotenv/config';
 
 export default {
 	schema: './src/lib/server/schema.ts',
 	out: './migrations',
+	driver: 'libsql',
+	dbCredentials: {
+		url: process.env.DATABASE_URL || '',
+	},
 	breakpoints: true,
 } satisfies Config;
