82a0f43513051eb8393f129d65833b3424827e63
Leo Mendoza Jr <dev@skyturtle.io>
2023-08-28 20:23:43 -0400

    a bunch of stuff, i don't know

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,4 +3,8 @@ import type { Config } from 'drizzle-kit';
 export default {
 	schema: './src/lib/server/schema.ts',
 	out: './drizzle',
+	driver: 'libsql',
+	dbCredentials: {
+		url: 'http://127.0.0.1:8080',
+	},
 } satisfies Config;
--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 
 export const libsqlClient = createClient({
-	url: 'file:test/main.db',
+	url: 'http://127.0.0.1:8080',
 });
 
 export const db = drizzle(libsqlClient);
