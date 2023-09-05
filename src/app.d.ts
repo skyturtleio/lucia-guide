1d6f44f6f7ac9baa0c5bf0a4c7fd6883de2ef902
Leo Mendoza Jr <dev@skyturtle.io>
2023-09-05 13:14:02 -0700

    fix app type declaratio file

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,6 +1,5 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
-/// <references types="lucia" />
 declare global {
 	namespace App {
 		// interface Error {}
@@ -10,6 +9,10 @@ declare global {
 		// interface PageData {}
 		// interface Platform {}
 	}
+}
+
+/// <reference types="lucia" />
+declare global {
 	namespace Lucia {
 		type Auth = import('$lib/server/lucia').Auth;
 		type DatabaseUserAttributes = {
