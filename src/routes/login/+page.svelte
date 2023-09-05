249d30c576bb3d02b9bbedcee3c850c335ce8301
Leo Mendoza Jr <dev@skyturtle.io>
2023-09-05 16:14:52 -0700

    add login page

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+</script>
+
+<h1>Sign In</h1>
+
+<form method="POST" use:enhance>
+	<label for="username">
+		<span>Username</span>
+		<input name="username" id="username" />
+	</label>
+	<label for="password">
+		<span>Password</span>
+		<input name="password" id="password" />
+		<button type="submit">Submit</button>
+	</label>
+</form>
+
+<a href="/signup">Don't have an account? Sign up</a>
