811b5b03c06b24e6625a563674c57d76dbead222
Leo Mendoza Jr <dev@skyturtle.io>
2023-09-13 17:43:08 -0700

    add startup function for server hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,16 @@
 import { auth } from '$lib/server/lucia';
 import type { Handle } from '@sveltejs/kit';
 
+const startupCode = () => {
+	console.log('I only run once, Im outside the handle function');
+	// place code that you want to run on startup here
+	// e.g., migrations, db connections
+};
+
+startupCode();
+
 export const handle: Handle = async ({ event, resolve }) => {
+	console.log('server hook: I run every time');
 	event.locals.auth = auth.handleRequest(event);
 	return await resolve(event);
 };
