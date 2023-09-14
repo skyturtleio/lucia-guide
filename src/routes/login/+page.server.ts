ae6677d41d9995e9bf39dfaf34236e1914d95244
Leo Mendoza Jr <dev@skyturtle.io>
2023-09-14 16:59:16 -0700

    change login to use only email and password

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -6,19 +6,22 @@ import type { Actions, PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	const session = await locals.auth.validate();
-	if (session) throw redirect(302, '/');
+	if (session) {
+		if (!session.user.emailVerified) throw redirect(302, '/email-verification');
+		throw redirect(302, '/');
+	}
 	return {};
 };
 
 export const actions: Actions = {
 	default: async ({ request, locals }) => {
 		const formData = await request.formData();
-		const username = formData.get('username');
+		const email = formData.get('email');
 		const password = formData.get('password');
 
-		if (typeof username !== 'string' || username.length < 1 || username.length > 31) {
+		if (typeof email !== 'string' || email.length < 1 || email.length > 31) {
 			return fail(400, {
-				message: 'invalid username',
+				message: 'invalid email',
 			});
 		}
 
@@ -30,7 +33,7 @@ export const actions: Actions = {
 
 		try {
 			// find user by key and validate the password
-			const key = await auth.useKey('username', username.toLowerCase(), password);
+			const key = await auth.useKey('email', email.toLowerCase(), password);
 			const session = await auth.createSession({ userId: key.userId, attributes: {} });
 
 			locals.auth.setSession(session);
