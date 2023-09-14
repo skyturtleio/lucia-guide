e023fd206d8ad9d9c3b9788592c0cc3cafdec93a
Leo Mendoza Jr <dev@skyturtle.io>
2023-09-14 11:54:10 -0700

    add link to docs for querying

--- src/lib/server/database.ts
+++ src/lib/server/database.ts
@@ -11,4 +11,15 @@ export const client = createClient({
 	url: process.env.DATABASE_URL || '',
 });
 
+/**
+ * At first I didn't have `{ schema }` passed as the second argument.
+ * Without it, I wasn't getting completions for `db.query....`
+ *
+ * From the docs:
+ * https://orm.drizzle.team/docs/rqb#querying
+ * > Relational queries are an extension to Drizzle's original query builder.
+ * > You need to provide all tables and relations from your schema file/files
+ * > upon drizzle() initialization and then just use the db.query API.
+ */
+
 export const db = drizzle(client, { schema });
