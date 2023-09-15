c48b6a3bca3000ea34bf84212fe9a5cefeac21ab
Leo Mendoza Jr <dev@skyturtle.io>
2023-09-14 17:02:45 -0700

    add email-verification page

---
+++ src/routes/email-verification/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+</script>
+
+<h1>Email verification</h1>
+<p>Your email verification link was sent to your inbox.</p>
+<p>For development, this is `console`</p>
+<h2>Resend verification link</h2>
+<form method="post" use:enhance>
+	<button type="submit">Resend</button>
+</form>
